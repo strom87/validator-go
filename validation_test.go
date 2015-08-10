ba963c4358eeb91f6e85d6cb676fbd2ea1019d08
Daniel Ström <daniel.strom.87@gmail.com>
2015-08-10 21:02:31 +0200

    some tests

--- validation_test.go
+++ validation_test.go
@@ -5,47 +5,90 @@ import (
 	"testing"
 )
 
-type TestStruct struct {
-	Int1    int     `validator:"min:3|max:2"`
-	Int2    int     `validator:"min:3|max:2"`
-	Float1  float64 `validator:"min:5.5|max:4.2"`
-	Float2  float64 `validator:"min:5.5|max:4.2"`
-	String1 string  `validator:"min:4|max:3"`
-	String2 string  `validator:"min:4|max:3"`
+type function func(p *Property, value string) error
+
+type minTestStruct struct {
+	v1       int     `validator:"min:200"`
+	v2       string  `validator:"min:4"`
+	v3       float64 `validator:"min:10.5"`
+	expected int
 }
 
-func TestMin(t *testing.T) {
-	errMsg = nil
-	ts := TestStruct{2, 3, 5.4, 5.6, "123", "1234"}
-	obj := reflect.ValueOf(ts)
+type maxTestStruct struct {
+	v1       int     `validator:"max:10"`
+	v2       string  `validator:"max:4"`
+	v3       float64 `validator:"max:10.5"`
+	expected int
+}
 
-	for i := 0; i < obj.NumField(); i++ {
-		p := NewProperty(obj.Field(i), obj.Type().Field(i))
+type betweenTestStruct struct {
+	v1       int     `validator:"between:15,20"`
+	v2       int     `validator:"between:15,20"`
+	v3       float64 `validator:"between:100,200"`
+	v4       float64 `validator:"between:100,200"`
+	v5       string  `validator:"between:3,6"`
+	v6       string  `validator:"between:3,6"`
+	expected int
+}
 
-		for _, v := range p.Validations {
-			min(p, v.Value)
-		}
+func TestMinF(t *testing.T) {
+	initialize("en")
+	values := []minTestStruct{
+		{199, "Bob", 10.49, 3},
+		{300, "Jhonny", 1042.95, 0},
+		{-2, "Brit", 10.5, 1},
 	}
 
-	if errMsg["Int1"] == nil || errMsg["Float1"] == nil || errMsg["String1"] == nil {
-		t.Error("Int1 is not greater than 3 or Float1 is greater then 5.4 or String1 not greater than 4")
+	for _, v := range values {
+		testRunner(v, min)
+		if len(errMsg) != v.expected {
+			t.Error("Expected", v.expected, "got", len(errMsg))
+		}
+		errMsg = nil
 	}
 }
 
-func TestMax(t *testing.T) {
-	errMsg = nil
-	ts := TestStruct{2, 3, 5.4, 5.6, "123", "1234"}
-	obj := reflect.ValueOf(ts)
-
-	for i := 0; i < obj.NumField(); i++ {
-		p := NewProperty(obj.Field(i), obj.Type().Field(i))
-
-		for _, v := range p.Validations {
-			max(p, v.Value)
-		}
+func TestMaxF(t *testing.T) {
+	initialize("en")
+	values := []maxTestStruct{
+		{11, "Jonny", 10.511, 3},
+		{10, "Bobb", 10.5, 0},
+		{-200, "abcdefg", 10.499, 1},
 	}
 
-	if errMsg["Int2"] == nil || errMsg["Float2"] == nil || errMsg["String2"] == nil {
-		t.Error("Int2 is not less then 2 or Float2 is not less then 5.5 or String2 is not less then 3")
+	for _, v := range values {
+		testRunner(v, max)
+		if len(errMsg) != v.expected {
+			t.Error("Expected", v.expected, "got", len(errMsg))
+		}
+		errMsg = nil
+	}
+}
+
+func TestBetweenF(t *testing.T) {
+	initialize("en")
+	values := []betweenTestStruct{
+		{17, 2000, 175.2, 99.99, "Bob", "Jonny", 3},
+		{16, 19, 199.99, 100.1, "Mats", "Jonny", 0},
+		{15, 20, 200.0, 100.0, "Max", "Johnnys", 6},
+	}
+
+	for _, v := range values {
+		testRunner(v, between)
+		if len(errMsg) != v.expected {
+			t.Error("Expected", v.expected, "got", len(errMsg))
+		}
+		errMsg = nil
+	}
+}
+
+func testRunner(obj interface{}, fn function) {
+	data := reflect.ValueOf(obj)
+	for i := 0; i < data.NumField(); i++ {
+		p := NewProperty(data.Field(i), data.Type().Field(i))
+
+		for _, v := range p.Validations {
+			fn(p, v.Value)
+		}
 	}
 }
